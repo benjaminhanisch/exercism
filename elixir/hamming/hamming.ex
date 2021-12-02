6228a6aa04e91b3cd48aafecc22617d1f90b8e42
Ben Hanisch <benjamin.hanisch@gmail.com>
2021-12-02 09:03:02 -0600

    files for hamming distance exercise

---
+++ elixir/hamming/hamming.ex
@@ -0,0 +1,23 @@
+defmodule Hamming do
+  @doc """
+  Returns number of differences between two strands of DNA, known as the Hamming Distance.
+
+  ## Examples
+
+  iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
+  {:ok, 4}
+  """
+  @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
+  def hamming_distance(strand1, strand2) when length(strand1) != length(strand2) do # using that guard, yo.
+    {:error, "Lists must be of the same type"}
+  end
+
+  @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
+  def hamming_distance(strand1, strand2) when length(strand1) == length(strand2) do # using that guard again, yo.
+    {:ok,
+      strand1
+      |> Enum.zip(strand2)
+      |> Enum.count(fn { a, b } -> a !== b end)
+    }
+  end
+end
